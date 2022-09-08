cc96013c1f35a66037255f61e7a0abfd05593bee
Pluralsight <Pluralsight@DESKTOP-KHCEUB9>
2022-09-08 07:30:33 -0400

    Project state at the end of module 3

---
+++ FlightPlanApi/Models/FlightPlan.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FlightPlanApi.Models
+{
+    public class FlightPlan
+    {
+        [JsonPropertyName("flight_plan_id")]
+        public string FlightPlanId { get; set; }
+
+        [JsonPropertyName("aircraft_identification")]
+        public string AircraftIdentification { get; set; }
+
+        [JsonPropertyName("aircraft_type")]
+        public string AircraftType { get; set; }
+
+        [JsonPropertyName("airspeed")]
+        public int Airspeed { get; set; }
+
+        [JsonPropertyName("altitude")]
+        public int Altitude { get; set; }
+
+        [JsonPropertyName("flight_type")]
+        public string FlightType { get; set; }
+
+        [JsonPropertyName("fuel_hours")]
+        public int FuelHours { get; set; }
+
+        [JsonPropertyName("fuel_minutes")]
+        public int FuelMinutes { get; set; }
+
+        [JsonPropertyName("departure_time")]
+        public DateTime DepartureTime { get; set; }
+
+        [JsonPropertyName("estimated_arrival_time")]
+        public DateTime ArrivalTime { get; set; }
+
+        [JsonPropertyName("departing_airport")]
+        public string DepartureAirport { get; set; }
+
+        [JsonPropertyName("arrival_airport")]
+        public string ArrivalAirport { get; set; }
+
+        [JsonPropertyName("route")]
+        public string Route { get; set; }
+
+        [JsonPropertyName("remarks")]
+        public string Remarks { get; set; }
+
+        [JsonPropertyName("number_onboard")]
+        public int NumberOnBoard { get; set; }
+    }
+}
