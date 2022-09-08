23ade1e45508418df571828af97ed52a8d766845
Pluralsight <Pluralsight@DESKTOP-KHCEUB9>
2022-09-08 09:12:19 -0400

    Complete Module 2 Code

---
+++ FlightPlanApi/Data/IDatabaseAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FlightPlanApi.Models;
+
+namespace FlightPlanApi.Data
+{
+    public interface IDatabaseAdapter
+    {
+        Task<List<FlightPlan>> GetAllFlightPlans();
+
+        Task<FlightPlan> GetFlightPlanById(string flightPlanId);
+
+        Task<bool> FileFlightPlan(FlightPlan flightPlan);
+
+        Task<bool> UpdateFlightPlan(string flightPlanId, FlightPlan flightPlan);
+
+        Task<bool> DeleteFlightPlanById(string flightPlanId);
+    }
+}
