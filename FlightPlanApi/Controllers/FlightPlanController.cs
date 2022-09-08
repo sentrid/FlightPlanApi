4c3d66dd016b58185281833e1ed994ed861a0e6a
Pluralsight <Pluralsight@DESKTOP-KHCEUB9>
2022-09-08 07:23:57 -0400

    Project state at the end of module 2

---
+++ FlightPlanApi/Controllers/FlightPlanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanApi.Controllers
+{
+    [Route("api/v1/flightplan")]
+    [ApiController]
+    public class FlightPlanController : ControllerBase
+    {
+    }
+}
--- FlightPlanApi/Program.cs
+++ FlightPlanApi/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,6 +16,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(config =>
+{
+    config
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 
