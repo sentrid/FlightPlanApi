a8dedb396479ca0d5106b28882ecd38a48dad711
Pluralsight <Pluralsight@DESKTOP-KHCEUB9>
2022-09-13 09:28:21 -0400

    Finished Module 3 code

--- FlightPlanApi/Data/MongoDbDatabase.cs
+++ FlightPlanApi/Data/MongoDbDatabase.cs
@@ -4,8 +4,108 @@ using FlightPlanApi.Models;
 
 namespace FlightPlanApi.Data
 {
-    public class MongoDbDatabase
+    public class MongoDbDatabase : IDatabaseAdapter
     {
+        public async Task<List<FlightPlan>> GetAllFlightPlans()
+        {
+            var collection = GetCollection("pluralsight", "flight_plans");
+            var documents = collection.Find(_ => true).ToListAsync();
+
+            var flightPlanList = new List<FlightPlan>();
+
+            if (documents == null) return flightPlanList;
+
+            foreach (var document in await documents)
+            {
+                flightPlanList.Add(ConvertBsonToFlightPlan(document));
+            }
+
+            return flightPlanList;
+        }
+
+        public async Task<FlightPlan> GetFlightPlanById(string flightPlanId)
+        {
+            var collection = GetCollection("pluralsight", "flight_plans");
+            var flightPlanCursor = await collection.FindAsync(
+                Builders<BsonDocument>.Filter.Eq("flight_plan_id", flightPlanId));
+            var document = flightPlanCursor.FirstOrDefault();
+            var flightPlan = ConvertBsonToFlightPlan(document);
+
+            if (flightPlan == null)
+            {
+                return new FlightPlan();
+            }
+
+            return new FlightPlan();
+        }
+
+        public async Task<bool> FileFlightPlan(FlightPlan flightPlan)
+        {
+            var collection = GetCollection("pluralsight", "flight_plans");
+
+            var document = new BsonDocument
+            {
+                {"flight_plan_id", Guid.NewGuid().ToString("N") },
+                {"altitude", flightPlan.Altitude },
+                {"airspeed", flightPlan.Airspeed },
+                {"aircraft_identification", flightPlan.AircraftIdentification },
+                {"aircraft_type", flightPlan.AircraftType },
+                {"arrival_airport", flightPlan.ArrivalAirport },
+                {"flight_type", flightPlan.FlightType },
+                {"departing_airport", flightPlan.DepartureAirport },
+                {"departure_time", flightPlan.DepartureTime },
+                {"estimated_arrival_time", flightPlan.ArrivalTime },
+                {"route", flightPlan.Route },
+                {"remarks", flightPlan.Remarks },
+                {"fuel_hours", flightPlan.FuelHours },
+                {"fuel_minutes", flightPlan.FuelMinutes },
+                {"number_onboard", flightPlan.NumberOnBoard }
+            };
+
+            try
+            {
+                await collection.InsertOneAsync(document);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> DeleteFlightPlanById(string flightPlanId)
+        {
+            var collection = GetCollection("pluralsight", "flight_plans");
+            var result = await collection.DeleteOneAsync(
+                Builders<BsonDocument>.Filter.Eq("flight_plan_id", flightPlanId));
+
+            return result.DeletedCount > 0;
+        }
+
+        public async Task<bool> UpdateFlightPlan(string flightPlanId, FlightPlan flightPlan)
+        {
+            var collection = GetCollection("pluralsight", "flight_plans");
+            var filter = Builders<BsonDocument>.Filter.Eq("flight_plan_id", flightPlanId);
+            var update = Builders<BsonDocument>.Update
+                .Set("altitude", flightPlan.Altitude)
+                .Set("airspeed", flightPlan.Airspeed)
+                .Set("aircraft_identification", flightPlan.AircraftIdentification)
+                .Set("aircraft_type", flightPlan.AircraftType)
+                .Set("arrival_airport", flightPlan.ArrivalAirport)
+                .Set("flight_type", flightPlan.FlightType)
+                .Set("departing_airport", flightPlan.DepartureAirport)
+                .Set("departure_time", flightPlan.DepartureTime)
+                .Set("estimated_arrival_time", flightPlan.ArrivalTime)
+                .Set("route", flightPlan.Route)
+                .Set("remarks", flightPlan.Remarks)
+                .Set("fuel_hours", flightPlan.FuelHours)
+                .Set("fuel_minutes", flightPlan.FuelMinutes)
+                .Set("numberOnBoard", flightPlan.NumberOnBoard);
+            var result = await collection.UpdateOneAsync(filter, update);
+
+            return result.ModifiedCount > 0;
+        }
 
         private IMongoCollection<BsonDocument> GetCollection(
             string databaseName, string collectionName)
